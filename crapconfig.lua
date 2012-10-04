ab08f2473a6e90d123a1ffa8a759076b8b38d576
Jimmy Hedman <jimmy.hedman@southpole.se>
2012-10-04 07:15:34 +0200

    Gave it different upload and download speeds.
    - Could now have different upload- and download speeds.
    - Added which interface that is closes to server and which is closesest
      to client.

--- crapconfig.lua
+++ crapconfig.lua
@@ -1,3 +1,5 @@
-presets = { ["2G"] = { bandwidth = 3000, latency = 100 },
-	    ["3G"] =  { bandwidth = 7000, latency = 80 },
+presets = { ["2G"] = { bandwidth = {["d"] =3000, ["u"] = 1000 }, latency = 100 },
+	    ["3G"] =  { bandwidth = {["d"] = 7000, ["u"] = 1500 }, latency = 80 },
 	    }
+clientsideinterface = "br-lan"
+serversideinterface = "eth0.2"
